1d35046f3ee6fe965a85f5681b6dc5757820e0db
toolboxal <82962687+toolboxal@users.noreply.github.com>
2023-08-03 14:24:52 +0800

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        primary: 'hsl(185, 75%, 39%)',
-        darkBlue: 'hsl(229, 23%, 23%)',
-        darkGray: 'hsl(227, 10%, 46%)',
+        primary: "hsl(185, 75%, 39%)",
+        darkBlue: "hsl(229, 23%, 23%)",
+        darkGray: "hsl(227, 10%, 46%)",
       },
       fontFamily: {
-        body: ['Kumbh Sans', 'sans-serif'],
+        body: ["Kumbh Sans", "sans-serif"],
       },
       fontSize: {
-        heading: '18px',
-        regular: '14px',
-        small: '10px',
+        heading: "18px",
+        regular: "14px",
+        small: "10px",
       },
     },
   },
-  plugins: ['prettier-plugin-tailwindcss'],
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+};
