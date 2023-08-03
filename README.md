5a7e3c0a0a9e8903066bced4553d2f0dcb44e1fd
toolboxal <82962687+toolboxal@users.noreply.github.com>
2023-08-03 14:17:11 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Frontend Mentor - Profile card component solution
+
+### Screenshot
+
+![](screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Tailwindcss
+- Mobile-first workflow
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'hsl(185, 75%, 39%)',
+        darkBlue: 'hsl(229, 23%, 23%)',
+        darkGray: 'hsl(227, 10%, 46%)',
+      },
+      fontFamily: {
+        body: ['Kumbh Sans', 'sans-serif'],
+      },
+      fontSize: {
+        heading: '18px',
+        regular: '14px',
+        small: '10px',
+      },
+    },
+  },
+  plugins: ['prettier-plugin-tailwindcss'],
+}
